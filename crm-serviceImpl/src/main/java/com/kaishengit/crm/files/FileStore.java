8823bedd24f7a52ed81f8b56c21c193678604f15
hoyt-wang <hoyt_wang@yahoo.com>
2017-11-20 19:18:52 +0800

    echart学习&七牛云云储服务搭建

---
+++ crm-serviceImpl/src/main/java/com/kaishengit/crm/files/FileStore.java
@@ -0,0 +1,7 @@
+package com.kaishengit.crm.files;
+
+/**
+ * Created by hoyt on 2017/11/18.
+ */
+public interface FileStore {
+}
---
+++ crm-serviceImpl/src/main/java/com/kaishengit/crm/files/QiniuFileStore.java
@@ -0,0 +1,8 @@
+package com.kaishengit.crm.files;
+
+/**
+ * Created by hoyt on 2017/11/20.
+ */
+
+public class QiniuFileStore {
+}
